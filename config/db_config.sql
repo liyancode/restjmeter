3382d4e208c6a505fc18c4af25f4782939b9a3a0
liyancode <liyan.code@gmail.com>
2016-12-07 15:38:39 +0800

    add jmeter_perfmon_metric table

--- config/db_config.sql
+++ config/db_config.sql
@@ -46,3 +46,38 @@ WITH (
 );
 ALTER TABLE jmeter_jmx_log
   OWNER TO postgres;
+
+
+CREATE TABLE jmeter_jmx_log
+(
+  id serial NOT NULL,
+  testid character varying NOT NULL,
+  status character varying NOT NULL,
+  time_stamp integer NOT NULL,
+  jmx_content character varying,
+  CONSTRAINT jmeter_jmx_log_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE jmeter_jmx_log
+  OWNER TO postgres;
+
+-- table for perfmon metrics
+CREATE TABLE jmeter_perfmon_metric
+(
+  id serial NOT NULL,
+  testid character varying NOT NULL,
+  metric_type VARCHAR not NULL ,-- cpu/memory/disk/network/tcp/jmx
+  label character varying NOT NULL,
+  time_stamp_str character varying NOT NULL,
+  value_str character varying NOT NULL,
+  CONSTRAINT jmeter_perfmon_metric_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE jmeter_perfmon_metric
+  OWNER TO postgres;
+
+CREATE INDEX ON jmeter_perfmon_metric (testid);
