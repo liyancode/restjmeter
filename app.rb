c53006b329d1b10635294d048ede5ba5b681ab55
liyancode <liyan.code@gmail.com>
2016-12-08 10:45:41 +0800

    dir

--- app.rb
+++ app.rb
@@ -59,8 +59,8 @@ Thread.new{
           RESTJMeter::Util.update_log_jmx_str_status(DB,test_id,'success')
 
           # save all perfmon metrics data to db
-          # RESTJMeter::Controller.save_all_perfmon_data_to_db(DB,test_id,test_results_dir)
-          RESTJMeter::Controller.save_all_perfmon_data_to_db(DB,"1612061001_LB_KI","/Users/yanli6/Desktop/1612061001_LB_KI")
+          RESTJMeter::Controller.save_all_perfmon_data_to_db(DB,test_id,test_results_dir)
+          # RESTJMeter::Controller.save_all_perfmon_data_to_db(DB,"1612061001_LB_KI","/Users/yanli6/Desktop/1612061001_LB_KI")
           # delete temp files.
           # RESTJMeter::Controller.delete_temp_jtl(jmeter_jtl_temp_file) # 161206: keep jtl file, not delete
           p "====Testing: #{test_id} end."
