16b3ddb1c97f6fd3dbec52dace70ed6c5230d6f4
liyancode <liyan.code@gmail.com>
2016-12-08 10:08:52 +0800

    add Logger

--- config.rb
+++ config.rb
@@ -11,3 +11,21 @@ set :port, CONFIG["SinatraPort"]
 
 # global constants
 DB = Sequel.connect(CONFIG["DataBase_Connection"])
+
+log_path =CONFIG["System_Log_File"]
+if log_path != nil
+  LOGGER = Logger.new(log_path)
+else
+  LOGGER = Logger.new(STDOUT)
+end
+
+LOGGERLEVEL = CONFIG["System_Log_Level"]
+if LOGGERLEVEL.eql? 'DEBUG'
+  LOGGER.level = Logger::DEBUG
+elsif LOGGERLEVEL.eql? 'INFO'
+  LOGGER.level = Logger::INFO
+elsif LOGGERLEVEL.eql? 'WARN'
+  LOGGER.level = Logger::WARN
+else
+  LOGGER.level = Logger::ERROR
+end
