08ac64b14e76723e657f86773d9360f4a61ebf48
liyancode <liyan.code@gmail.com>
2017-07-25 16:11:29 +0800

    add insert_function_test_result

--- app/util.rb
+++ app/util.rb
@@ -427,5 +427,15 @@ module RESTJMeter
             <stringProp name="RegexExtractor.match_number"></stringProp>
           </RegexExtractor><hashTree/>'
     end
+
+    # d
+    def Util.insert_function_test_result(db,test_id,status_code,response_body)
+      begin
+        db.fetch("insert into jmeter_function_test_result(testid,response_code,response_body,time_stamp) values('#{test_id}',#{status_code},'#{response_body}'#{Time.now})").insert
+      rescue Exception=>e
+        p e
+        LOGGER.error "Util.log_jmx_str #{e}"
+      end
+    end
   end
 end
